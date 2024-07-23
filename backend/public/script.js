ddabf0a76cac77f895867631c0926f360afb6eea
Abqariyahhh <abqariyahyakeen04@gmail.com>
2024-07-24 00:44:49 +0530

    updated

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,26 +1,5 @@
 const socket = io();
 let user = 'user1'; // Initial user
-const user1Messages = [
-    "Hello, how are you?",
-    "Did you receive my last message?",
-    "Let's meet up tomorrow!"
-];
-const user2Messages = [
-    "I'm good, thanks! How about you?",
-    "Yes, I did. I'll be there!",
-    "Great! See you then."
-];
-let user1Index = 0;
-let user2Index = 0;
-
-// Determine if the current user is User 1 or User 2
-socket.on('connect', () => {
-    if (document.querySelectorAll('.message').length === 0) {
-        user = 'user1';
-    } else {
-        user = 'user2';
-    }
-});
 
 // Send message on "Enter" key press or "Send" button click
 document.getElementById('send-button').addEventListener('click', sendMessage);
